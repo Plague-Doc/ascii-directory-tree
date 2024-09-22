5aeb2d1bc186752b35f28e38d7c61a04a9e83d83
Vass Norbert <vassnorbert20@gmail.com>
2024-09-22 17:28:03 +0200

    Add project files.

---
+++ ASCII Directory Tree/Folder.cs
@@ -0,0 +1,163 @@
+﻿using System.Text;
+
+namespace ASCII_Directory_Tree;
+
+public class Folder
+{
+    private string Name { get; set; }
+    private string Path { get; set; }
+    private bool IsRoot { get; set; }
+    private int Depth { get; set; }
+    private List<Folder> Children { get; set; }
+    private List<string> Files { get; set; }
+    private int TotalContent { get; set; }
+    private List<string> Structure { get; set; }
+    public string Tree
+    {
+        get
+        {
+            StringBuilder tree = new();
+            foreach (var node in Structure)
+            {
+                tree.AppendLine(node);
+            }
+
+            return tree.ToString();
+        } 
+    }
+
+    public Folder(string path)
+    {
+        Name = path.TrimEnd('\\').Split('\\').Last();
+        Path = path;
+        IsRoot = true;
+        Depth = 0;
+        Children = GetChildren();
+        Files = GetFiles();
+        TotalContent = GetTotalContent();
+        Structure = GenerateStructure();
+    }
+
+    private Folder(string path, bool root, int depth)
+    {
+        Name = path.Split('\\').Last();
+        Path = path;
+        IsRoot = root;
+        Depth = IsRoot ? 0 : depth + 1;
+        Children = GetChildren();
+        Files = GetFiles();
+        TotalContent = GetTotalContent();
+        Structure = GenerateStructure();
+    }
+
+    private List<Folder> GetChildren()
+    {
+        List<string> paths = [.. Directory.GetDirectories(Path)];
+
+        List<Folder> children = [];
+
+        foreach (var path in paths)
+        {
+            children.Add(new Folder(path, false, Depth));
+        }
+
+        return children;
+    }
+
+    private List<string> GetFiles()
+    {
+        List<string> files = [.. Directory.GetFiles(Path)];
+
+        List<string> fileNames = [];
+
+        foreach (var file in files)
+        {
+            fileNames.Add(file.Split('\\').Last());
+        }
+
+        return fileNames;
+    }
+
+    private int GetTotalContent()
+    {
+        int total = 0;
+        total += Children.Count;
+        total += Files.Count;
+
+        foreach (var child in Children)
+        {
+            total += child.TotalContent;
+        }
+
+        return total;
+    }
+
+    private List<string> GenerateStructure()
+    {
+        List<string> tree = [];
+
+        if (IsRoot)
+        {
+            tree.Add($"~/{Name}/");
+        }
+
+        // Mapping files
+        for (int i = 0; i < Files.Count; i++)
+        {
+            if (i != Files.Count - 1 || Children.Count > 0)
+            {
+                tree.Add($"├── {Files[i]}");
+            }
+            else
+            {
+                tree.Add($"└── {Files[i]}");
+            }
+        }
+
+        var emptyDirs = Children
+            .Where(x => x.Children.Count == 0 && x.Files.Count == 0)
+            .ToList();
+
+        var fullDirs = Children.Except(emptyDirs).ToList();
+
+        // Mapping empty directories
+        for (int i = 0; i < emptyDirs.Count; i++)
+        {
+            if (i != emptyDirs.Count - 1 || fullDirs.Count > 0)
+            {
+                tree.Add($"├── {emptyDirs[i].Name}/");
+            }
+            else
+            {
+                tree.Add($"└── {emptyDirs[i].Name}/");
+            }
+        }
+
+        // Mapping directories with content
+        for (int i = 0; i < fullDirs.Count; i++)
+        {
+            if (i != fullDirs.Count - 1)
+            {
+                tree.Add($"├── {fullDirs[i].Name}/");
+            }
+            else
+            {
+                tree.Add($"└── {fullDirs[i].Name}/");
+            }
+
+            foreach (var node in fullDirs[i].Structure)
+            {
+                if (i != fullDirs.Count - 1)
+                {
+                    tree.Add($"│   {node}");
+                }
+                else
+                {
+                    tree.Add($"    {node}");
+                }
+            }
+        }
+
+        return tree;
+    }
+}
---
+++ ASCII Directory Tree/Program.cs
@@ -0,0 +1,129 @@
+﻿using ASCII_Directory_Tree;
+using System.Text;
+
+const string art = """
+                _____  _____ _____ _____     _______ _____  ______ ______ 
+         /\    / ____|/ ____|_   _|_   _|   |__   __|  __ \|  ____|  ____|
+        /  \  | (___ | |      | |   | |        | |  | |__) | |__  | |__   
+       / /\ \  \___ \| |      | |   | |        | |  |  _  /|  __| |  __|  
+      / ____ \ ____) | |____ _| |_ _| |_       | |  | | \ \| |____| |____ 
+     /_/    \_\_____/ \_____|_____|_____|      |_|  |_|  \_\______|______|
+
+                                                                         
+    """;
+
+string path;
+
+if (args.Length > 0)
+{
+    if (!Directory.Exists(args[0]))
+    {
+        Environment.Exit(0);
+    }
+    else if (Directory.GetDirectories(args[0]).Length + Directory.GetFiles(args[0]).Length == 0)
+    {
+        Environment.Exit(0);
+    }
+
+    path = args[0].Replace('/', '\\');
+}
+else
+{
+    Console.WriteLine(art);
+    Console.WriteLine("Input the full path to the directory:");
+    while (true)
+    {
+        string? input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.WriteLine("Input the full path to the directory:");
+            continue;
+        }
+        else if (!Directory.Exists(input))
+        {
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.WriteLine("Provided directory doesn't exist.");
+            Console.WriteLine("Please try again:");
+            continue;
+        }
+        else if (Directory.GetDirectories(input).Length + Directory.GetFiles(input).Length == 0)
+        {
+            Console.Clear();
+            Console.WriteLine(art);
+            Console.WriteLine("There's nothing inside the provided directory.");
+            Console.WriteLine("Please try again:");
+            continue;
+        }
+
+        path = input.Replace('/', '\\');
+        break;
+    }
+}
+
+Folder root = new(path);
+
+if (args.Length > 0)
+{
+    if (File.Exists($"{path}\\..\\ASCII_File_Tree.txt"))
+    {
+        File.Delete($"{path}\\..\\ASCII_File_Tree.txt");
+    }
+
+    using FileStream fs = File.Create($"{path}\\..\\ASCII_File_Tree.txt");
+
+    Byte[] content = new UTF8Encoding(true).GetBytes(root.Tree);
+    fs.Write(content, 0, content.Length);
+}
+else
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine($"ASCII Tree of provided directory:");
+        Console.WriteLine(path);
+        Console.WriteLine();
+        Console.WriteLine(root.Tree);
+        Console.Write("\nCopy ASCII Tree to a txt file? (y/N) ");
+
+        var input = Console.ReadKey(true).Key;
+
+        if (input == ConsoleKey.Enter || input == ConsoleKey.N)
+        {
+            Console.WriteLine("N");
+            break;
+        }
+        else if (input == ConsoleKey.Y)
+        {
+            Console.WriteLine("Y");
+
+            if (File.Exists($"{path}\\..\\ASCII_File_Tree.txt"))
+            {
+                File.Delete($"{path}\\..\\ASCII_File_Tree.txt");
+            }
+
+            using FileStream fs = File.Create($"{path}\\..\\ASCII_File_Tree.txt");
+            {
+                Byte[] content = new UTF8Encoding(true).GetBytes(root.Tree);
+                fs.Write(content, 0, content.Length);
+            }
+
+            List<string> pathList = [.. path.Split('\\')];
+
+            pathList.RemoveAt(pathList.Count - 1);
+
+            pathList.Add("ASCII_File_Tree.txt");
+
+            string filePath = string.Join('\\', pathList);
+
+            Console.WriteLine($"Text file has been created at '{filePath}'");
+            break;
+        }
+    }
+
+    Console.Write("\nPress any key to exit...");
+    Console.ReadKey(true);
+}
